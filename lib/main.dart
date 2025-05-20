e599087a5854c26be953bd56065d1782baa99d6b
hani <hanisoft2022@gmail.com>
2025-05-20 20:06:05 +0900

    before login screen validation config

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
@@ -10,5 +11,8 @@ import 'app.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 4000);
+
   runApp(ProviderScope(child: MyApp()));
 }
